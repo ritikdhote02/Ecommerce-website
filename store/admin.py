a3d5ab2ba1c6854e515b438d42a3a847fa7fe4ea
Ritik Dhote <89073677+imRocky02@users.noreply.github.com>
2022-07-28 00:08:42 +0530

    Complete and first attempt code of ecommerce project

---
+++ store/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Product, Variation, ReviewRating, ProductGallery
+import admin_thumbnails
+
+
+@admin_thumbnails.thumbnail('image')
+class ProductGalleryInline(admin.TabularInline):
+    model = ProductGallery
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
+    prepopulated_fields = {'slug': ('product_name',)}
+    inlines = [ProductGalleryInline]
+
+class variationAdmin(admin.ModelAdmin):
+     list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+     list_editable = ('is_active',)
+     list_filter =  ('product', 'variation_category', 'variation_value')
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, variationAdmin)
+admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
